34210e435be1f8cfe31ba310f9c3a968132c586f
Nickolas Nikolic <nickolasnikolic@gmail.com>
2016-01-23 04:39:03 -0600

    ideas now in the url

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,18 +1,16 @@
 var wdApp = angular.module('wdApp', ['ui.router', 'ngSanitize', 'angular.filter'])
-  .config(['$stateProvider',
-    function($stateProvider) {
-      $stateProvider
+  .config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
 
-      .state('home', {
-        url: '/',
+      $urlRouterProvider.otherwise('/ideas');
+
+      $stateProvider.state('home', {
+        url: '/ideas',
         templateUrl: 'templates/home.html',
         controller: 'HomeController',
         onEnter: function($state) {
-          console.log('home');
-          //if the credentials are not set
-          //route back to
+          console.log('home/ideas');
         }
-      })
-
+      });
     }
   ]);
